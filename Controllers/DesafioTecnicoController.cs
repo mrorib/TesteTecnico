d174977154d5e8bc1985c03b421594b382b21706
Windows <Windows@PC-GAMER-MARCEL>
2023-07-31 14:27:56 -0300

    Add project files.

---
+++ Controllers/DesafioTecnicoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesteTecnico.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DesafioTecnicoController : ControllerBase
+    {
+        public DesafioTecnicoController() { }
+
+        [HttpGet(Name = "NumeroPorExtenso/{numero}")]
+        public string GetNumeroPorExtenso(int numero)
+        {
+            return null;
+        }
+    }
+}
