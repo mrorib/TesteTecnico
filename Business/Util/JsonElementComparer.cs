b8bae01d9712d5397f8110e4f0634e731be0d2b3
Marcelo Rodrigo Ribeiro <doutor.marcelo@outlook.com.br>
2023-08-02 19:28:25 -0300

    Implementado Testes unitários e corrigido falhas encontradas

---
+++ Business/Util/JsonElementComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+
+namespace TesteTecnico.Business.Util
+{
+    public class JsonElementComparer : IEqualityComparer<JsonElement>
+    {
+        public bool Equals(JsonElement x, JsonElement y)
+        {
+            return (Convert.ToString(x) + x.ValueKind) == (Convert.ToString(y) + y.ValueKind);
+        }
+
+        public int GetHashCode([DisallowNull] JsonElement obj)
+        {
+            return (Convert.ToString(obj) + obj.ValueKind).GetHashCode();
+        }
+    }
+}
